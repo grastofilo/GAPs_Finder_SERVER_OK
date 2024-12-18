7f5b0a5403a9a7594a90748093d46216df9df2a7
grastofilo <mail@lucaloiacono.com>
2024-12-18 20:52:08 +0100

    Update GAPs_Finder_v1_02.py

--- GAPs_Finder_v1_02.py
+++ GAPs_Finder_v1_02.py
@@ -243,8 +243,9 @@ def yfinance_func(nome_ticker):
     #else:
     
     dati_storici=None;splits=None
+    yf.clear_cache()
     ticker = yf.Ticker(nome_ticker.upper())
-    dati_storici = ticker.history(period="max",timeout = 30,cache=False)  # dati periodo massimo disponibile
+    dati_storici = ticker.history(period="max",timeout = 30)  # dati periodo massimo disponibile
     splits = ticker.splits
             
         #with open(cache_file, "wb") as f:
