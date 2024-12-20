b8e89d1f0eb71e2ea5d75a3632f9f63eddbb6cc0
grastofilo <mail@lucaloiacono.com>
2024-12-20 12:53:58 +0100

    Update subprocess_001.py

--- subprocess_001.py
+++ subprocess_001.py
@@ -1,25 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 20 12:12:56 2024
+import os
 
-@author: ninni
-"""
-
-
-import sys
-import yfinance as yf
-
-def run_task(nome_ticker):
-    print(f"Dati ricevuti dallo script: {nome_ticker}")
-    
-    #ticker = yf.Ticker(nome_ticker.upper())
-    #dati_storici = ticker.history(period="max")
-    
-    # Aggiungi una gestione del risultato da ritornare
-    return f"questo è il valore del subprocess_001 {nome_ticker}"
+def run_task():
+    data = os.getenv('DATA')  # Ottieni il valore della variabile d'ambiente
+    print(f"Dati ricevuti dallo script: {data}")
+    return f"Risultato: {data}"
 
 if __name__ == "__main__":
-    nome_ticker = sys.argv[1]  # Recupera l'argomento passato dalla riga di comando
-    result = run_task(nome_ticker)
-    print(result)  # Stampa il risultato, che sarà catturato dal subprocess.run()
+    print(run_task())
