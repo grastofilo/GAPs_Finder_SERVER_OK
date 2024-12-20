b1b0fa5e70c24a30663e2edfa5e04ac48088fecd
grastofilo <mail@lucaloiacono.com>
2024-12-20 12:28:19 +0100

    Update subprocess_001.py

--- subprocess_001.py
+++ subprocess_001.py
@@ -10,9 +10,11 @@ import sys
 
 def run_task(nome_ticker):
     
-    print(f"Dati ricevuti dallo script: {nome_ticker}")
-    return f"Risultato: il tuo ticker è {nome_ticker}"
-
+    ticker = yf.Ticker(nome_ticker.upper())
+    dati_storici =  ticker.history(period="max")
+    
+    return f' questo è il valore del subprocess_001 dati_storici['Stock Splits'].sum()'
+    
 if __name__ == "__main__":
     nome_ticker = sys.argv[1]  # Recupera l'argomento passato dalla riga di comando
     print(run_task(nome_ticker))
