49ad1a8c93fb751a82b10708213307f2b3eced52
grastofilo <mail@lucaloiacono.com>
2024-12-20 12:21:49 +0100

    Add files via upload

---
+++ CHIAMA_apps.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 20 12:19:52 2024
+
+@author: ninni
+"""
+
+import streamlit as st
+import subprocess
+
+# Funzione per eseguire script.py passando dati come argomenti
+def run_external_script(nome_ticker):
+    result = subprocess.run(['python', 'subprocess_001.py', nome_ticker], capture_output=True, text=True)
+    return result.stdout
+
+# Usa la funzione in Streamlit
+if st.button('Esegui script'):
+    nome_ticker = "Dati di esempio"
+    output = run_external_script(nome_ticker)
+    st.write(output)
---
+++ subprocess_001.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 20 12:12:56 2024
+
+@author: ninni
+"""
+
+import sys
+
+def run_task(nome_ticker):
+    
+    print(f"Dati ricevuti dallo script: {nome_ticker}")
+    return f"Risultato: {nome_ticker}"
+
+if __name__ == "__main__":
+    nome_ticker = sys.argv[1]  # Recupera l'argomento passato dalla riga di comando
+    print(run_task(nome_ticker))
