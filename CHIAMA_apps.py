179790893ac6242664d7a6c418a0257eb78635af
grastofilo <mail@lucaloiacono.com>
2024-12-20 12:45:23 +0100

    Update CHIAMA_apps.py

--- CHIAMA_apps.py
+++ CHIAMA_apps.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 20 12:19:52 2024
-
-@author: ninni
-"""
-
 import streamlit as st
 import subprocess
 
 # Funzione per eseguire script.py passando dati come argomenti
 def run_external_script(nome_ticker):
-    result = subprocess.run(['python', 'subprocess_001.py', nome_ticker], capture_output=True, text=True)
-    return result.stdout
+    result = subprocess.run(
+        ['python', 'subprocess_001.py', nome_ticker],
+        capture_output=True,  # Cattura l'output
+        text=True  # Gestisci l'output come stringa
+    )
+    return result.stdout  # Restituisce l'output del subprocess
 
 # Usa la funzione in Streamlit
 if st.button('Esegui script'):
-    nome_ticker = "SONN"
+    nome_ticker = "AAPL"  # Esempio di ticker
     output = run_external_script(nome_ticker)
-    st.write(output)
+    st.write(output)  # Visualizza l'output nell'app Streamlit
