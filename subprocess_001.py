95e2b530521f67acf708c31c036d3d63abfd4f57
grastofilo <mail@lucaloiacono.com>
2024-12-20 12:43:09 +0100

    Update subprocess_001.py

--- subprocess_001.py
+++ subprocess_001.py
@@ -6,19 +6,20 @@ Created on Fri Dec 20 12:12:56 2024
 @author: ninni
 """
 
+
 import sys
+import yfinance as yf
 
 def run_task(nome_ticker):
-
     print(f"Dati ricevuti dallo script: {nome_ticker}")
+    
     ticker = yf.Ticker(nome_ticker.upper())
-    dati_storici =  ticker.history(period="max")
-    print(f"questo è il valore del subprocess_001 {dati_storici['Stock Splits'].sum()}")
-
+    dati_storici = ticker.history(period="max")
     
-    
+    # Aggiungi una gestione del risultato da ritornare
     return f"questo è il valore del subprocess_001 {dati_storici['Stock Splits'].sum()}"
-    
+
 if __name__ == "__main__":
     nome_ticker = sys.argv[1]  # Recupera l'argomento passato dalla riga di comando
-    print(run_task(nome_ticker))
+    result = run_task(nome_ticker)
+    print(result)  # Stampa il risultato, che sarà catturato dal subprocess.run()
