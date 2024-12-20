822f758683a30bca9eecdc162f67232643c743b4
grastofilo <mail@lucaloiacono.com>
2024-12-20 12:24:14 +0100

    Update subprocess_001.py

--- subprocess_001.py
+++ subprocess_001.py
@@ -11,7 +11,7 @@ import sys
 def run_task(nome_ticker):
     
     print(f"Dati ricevuti dallo script: {nome_ticker}")
-    return f"Risultato: {nome_ticker}"
+    return f"Risultato: il tuo ticker è {nome_ticker}"
 
 if __name__ == "__main__":
     nome_ticker = sys.argv[1]  # Recupera l'argomento passato dalla riga di comando
